22abaddf551a8bd80264cc93eac26323b88462d6
Az195 <aziz047k@gmail.com>
2024-07-30 17:29:24 +0500

    quantity selector js added

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -26,7 +26,7 @@
         </div>
 
         <div class="product-quantity-container">
-          <select>
+          <select class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -61,17 +61,21 @@ document.querySelectorAll('.js-add-to-cart').
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
       let matchingItem;
+      const quantitySelector = document
+        .querySelector(`.js-quantity-selector-${productId}`);
+      const quantity = Number(quantitySelector.value);
+      
       cart.forEach((item) => {
         if(productId === item.productId){
           matchingItem = item;
         }
       });
       if (matchingItem) {
-        ++matchingItem.quantity;
+        matchingItem.quantity += quantity;
       } else {
         cart.push({
           productId: productId,
-          quantity: 1
+          quantity: quantity
         });
       }
       let cartQuantity = 0;
