79506e5da1e26c03059d7ede220f79186940df98
Az195 <aziz047k@gmail.com>
2024-07-30 10:14:04 +0500

    connecting products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,31 +1,4 @@
-const products = [
-  {
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090
-  },  {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-      stars: 4,
-      count: 127
-    },
-    priceCents: 2095
-  }, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-      stars: 4.5,
-      count: 56
-    },
-    priceCents: 799
-  }];
-
-  let productsHTML='';
+ let productsHTML='';
 
   products.forEach((product)=>{
   
@@ -49,7 +22,7 @@ const products = [
           </div>
 
           <div class="product-price">
-            $${product.priceCents/100}
+            $${(product.priceCents/100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
