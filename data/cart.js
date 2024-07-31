ca25949fe88dce2926f92168f4fb9509200c9065
Az195 <aziz047k@gmail.com>
2024-07-31 13:36:28 +0500

    Moving functions to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,21 @@
 export const cart = [];
+
+export function addToCart(productId){
+  let matchingItem;
+  const quantitySelector = document
+    .querySelector(`.js-quantity-selector-${productId}`);
+  const quantity = Number(quantitySelector.value);
+  cart.forEach((cartItem) => {
+    if(productId === cartItem.productId){
+      matchingItem = cartItem;
+    }
+  });
+  if (matchingItem) {
+    matchingItem.quantity += quantity;
+  } else {
+    cart.push({
+      productId,
+      quantity
+    });
+  }
+};
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
- import { cart } from '../data/cart.js';
+ import { cart, addToCart } from '../data/cart.js';
  import { products } from '../data/products.js';
  
  let productsHTML='';
@@ -59,34 +59,24 @@
 document.querySelector('.js-products-grid')
   .innerHTML=productsHTML;
 
+function updateCartQuantity(){
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  })
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
+}
+
 document.querySelectorAll('.js-add-to-cart').
   forEach((button) => {
     let notificationElementTimeoutId;
     button.addEventListener('click', () => {
       const {productId} = button.dataset;
-      let matchingItem;
-      const quantitySelector = document
-        .querySelector(`.js-quantity-selector-${productId}`);
-      const quantity = Number(quantitySelector.value);
-      cart.forEach((item) => {
-        if(productId === item.productId){
-          matchingItem = item;
-        }
-      });
-      if (matchingItem) {
-        matchingItem.quantity += quantity;
-      } else {
-        cart.push({
-          productId,
-          quantity
-        });
-      }
-      let cartQuantity = 0;
-      cart.forEach((item) => {
-        cartQuantity += item.quantity;
-      })
-      document.querySelector('.js-cart-quantity')
-        .innerHTML = cartQuantity;
+
+      addToCart(productId);
+      updateCartQuantity();
+     
       const notificationElement = document.querySelector(`
         .js-added-to-cart-${productId}`);
       notificationElement.classList.add
