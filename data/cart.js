48e1e30e11ee399db74ed8235a039638d4340ac9
Az195 <aziz047k@gmail.com>
2024-08-01 16:54:42 +0500

    when Items in the cart saved to the local storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,14 @@
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
+};
 
 export function addToCart(productId){
   let matchingItem;
@@ -24,7 +28,12 @@ export function addToCart(productId){
       quantity
     });
   }
+  saveToStorage();
 };
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function removeFromCart(productId) {
   const newCart = [];
@@ -33,6 +42,6 @@ export function removeFromCart(productId) {
       newCart.push(cartItem);
     }
   });
-
   cart=newCart;
+  saveToStorage()
 };
