b1f61ed452b9aae2c6432c1c25d23800f6561ae4
Az195 <aziz047k@gmail.com>
2024-08-06 20:41:08 +0500

    default export added to utils money js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import {cart, removeFromCart,
   calculateCartQuantity, updateQuantity} from "../data/cart.js";
 import {products} from "../data/products.js";
-import {formatCurrency} from './utils/money.js';
+import formatCurrency from './utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions} from '../data/deliveryOptions.js'
 
--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,4 @@
 export function formatCurrency(priceCents){
   return (priceCents/100).toFixed(2);
 }
+export default formatCurrency;
